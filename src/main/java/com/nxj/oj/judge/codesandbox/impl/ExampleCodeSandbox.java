6868dbf4b04dec4d97cc89f5381754d5b23fa0ff
tivvvvffffq <tivvvvffffq@gmail.com>
2024-01-25 14:13:23 +1100

    判题流程开发

--- src/main/java/com/nxj/oj/judge/codesandbox/impl/ExampleCodeSandbox.java
+++ src/main/java/com/nxj/oj/judge/codesandbox/impl/ExampleCodeSandbox.java
@@ -18,15 +18,18 @@ public class ExampleCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         List<String> inputList = executeCodeRequest.getInputList();
+
         ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
         executeCodeResponse.setOutputList(inputList);
         executeCodeResponse.setMessage("测试执行成功");
         executeCodeResponse.setStatus(QuestionSubmitStatusEnum.SUCCEED.getValue());
+
         JudgeInfo judgeInfo = new JudgeInfo();
         judgeInfo.setMessage(JudgeInfoMessageEnum.ACCEPTED.getText());
         judgeInfo.setMemory(100L);
         judgeInfo.setTime(100L);
         executeCodeResponse.setJudgeInfo(judgeInfo);
+
         System.out.println("示例代码沙箱");
         return executeCodeResponse;
     }
